c9e797a933a5b80a923c009e8fe9b321fecc54e4
krishnag0el <krishnagoel17170624@gmail.com>
2026-08-18 10:22:31 +0530

    low-pass filtering experiment

--- experiments/05_filtering/filtering.py
+++ experiments/05_filtering/filtering.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from spicy.signal import butter,filtfilt
+from scipy.signal import butter,filtfilt
 fs=1000
 duration=1
 N=int(duration*fs)
 t=np.arrange(N)/fs
-signal=np.sine(2*np.pi*50*t)+0.5*np.sine(2*np.pi*120*t)
-noise=0.5*np.sine(2*np.pi*300*t)
+signal=(np.sin(2*np.pi*50*t)+0.5*np.sin(2*np.pi*120*t))
+noise=0.5*np.sin(2*np.pi*300*t)
 noisy_signal=signal+noise
 
 cutoff=150
@@ -22,5 +22,5 @@ plt.ylabel("Amplitude")
 plt.title("Low-Pass Filtering")
 plt.legend()
 plt.grid()
-plt.savefig("filtered_signal.png",dpi=300,bbox_index=tight)
+plt.savefig("filtered_signal.png",dpi=300,bbox_inches="tight")
 plt.show()
